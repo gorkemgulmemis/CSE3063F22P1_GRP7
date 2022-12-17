46b0b66bf661503598d8af2be2bf2cdb5a37e787
Tarık Işın <118109974+TarikIsin@users.noreply.github.com>
2022-12-17 11:42:30 +0300

    Add files via upload

---
+++ iteration2/src/main/Main.java
@@ -0,0 +1,22 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws ParseException, IOException {
+
+        Student student = new Student();
+        student.createStudent();
+
+    }
+}
