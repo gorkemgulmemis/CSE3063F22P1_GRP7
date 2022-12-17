4d070cd83b70e60cb5246ad652d34adfc98ca96b
Abdullah Enes Dizer <118109934+AbdullahEnesDizer@users.noreply.github.com>
2022-12-17 12:00:03 +0300

    Update Transcript.java
    generateRandomLetterGrade()

--- iteration2/src/main/Transcript.java
+++ iteration2/src/main/Transcript.java
@@ -11,3 +11,12 @@ public class Transcript {
     public void setLetterGrade(String letterGrade) {
         this.letterGrade = letterGrade;
     }
+    public void generateRandomLetterGrade(){
+
+        Random random = new Random();
+
+        String[] grades = {"AA", "BA", "BB", "CB", "CC", "DC", "DD", "FD", "FF"};
+
+        String letterGrade = grades[random.nextInt(grades.length)];
+        setLetterGrade(letterGrade);
+    }
