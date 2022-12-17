55fa44daf5dd0f79e858ae4938368612e6076d15
mervanabursu <93670475+mervanabursu@users.noreply.github.com>
2022-12-17 11:59:52 +0300

    Update StudentProcess.java

--- iteration2/src/main/StudentProcess.java
+++ iteration2/src/main/StudentProcess.java
@@ -25,3 +25,35 @@ public class StudentProcess {
     public void setStudent(Student student) {
         this.student = student;
     }
+    public void createStudentJsonFile() throws ParseException{
+
+		String path = "C:\\Users\\90554\\eclipse-workspace\\Projemiz\\Student\\" + student.getStudentNumber() + ".json";
+		course = new Course();
+		advisor = new Advisor();
+		transcript = new Transcript();
+		JSONObject json = new JSONObject();
+
+		try {
+
+			json.put("Student Name ", student.getStudentArrayList().get(0).getName());
+			json.put("Student Year ", student.getStudentArrayList().get(0).getYear());
+			json.put("Student Semester ", student.getStudentArrayList().get(0).getSemester());
+			json.put("Student Number ", student.getStudentArrayList().get(0).getStudentNumber());
+			json.put("Student Title ", student.getStudentArrayList().get(0).getTitle());
+			json.put("Student Department ", student.getStudentArrayList().get(0).getDepartment());
+
+			course.takeCoursesFromInputFile(student.getStudentArrayList().get(0).getSemester() + 1);
+			course.takeLastSemesterCourseFromInputFile(student.getStudentArrayList().get(0).getSemester());
+
+			double totalGrade = 0;
+			double semesterTotalGrade = 0;
+			for(int i = 0; i < course.getCourseList().size(); i++) {
+				transcript.generateRandomLetterGrade();
+				String courseInfo = course.getCourseList().get(i).getCourseName() + " - " + transcript.getLetterGrade();
+				json.put("Course Name " + (i+1), courseInfo);
+
+				totalGrade +=  course.getCourseList().get(i).getCourseCredit() * transcript.gradeChangeToDouble(transcript.getLetterGrade());
+				if(i < course.getLastSemesterCourseList().size()) {
+           	        semesterTotalGrade += course.getLastSemesterCourseList().get(i).getCourseCredit() * transcript.gradeChangeToDouble(transcript.getLetterGrade());
+				}
+			}
