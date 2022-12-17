1d7f747e57693e220819224fa2705e3ec398ff27
mervanabursu <93670475+mervanabursu@users.noreply.github.com>
2022-12-17 11:50:45 +0300

    Update Person.java

--- iteration2/src/main/Person.java
+++ iteration2/src/main/Person.java
@@ -1,1 +1,40 @@
 
+class Person{
+
+    private String name;
+    private String department;
+    private String title;
+
+    public Person(){
+        super();
+    }
+
+    public Person(String name,String department,String title){
+        this.name=name;
+        this.department=department;
+        this.title=title;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
