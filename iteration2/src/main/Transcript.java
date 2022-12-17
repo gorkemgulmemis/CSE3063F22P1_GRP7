bd8572b202627a88e88e6b81da1659024b481966
mervanabursu <93670475+mervanabursu@users.noreply.github.com>
2022-12-17 11:55:33 +0300

    Create Transcript.java

---
+++ iteration2/src/main/Transcript.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class Transcript {
+
+    private String letterGrade;
+
+    public String getLetterGrade() {
+        return letterGrade;
+    }
+
+    public void setLetterGrade(String letterGrade) {
+        this.letterGrade = letterGrade;
+    }
