b808082ad9dafbd8bd0b97f3b14cde4d50998123
Tarık Işın <118109974+TarikIsin@users.noreply.github.com>
2023-01-08 23:21:42 +0300

    Update Transcript.py

--- iteration3/src/Transcript.py
+++ iteration3/src/Transcript.py
@@ -1,1 +1,20 @@
 
+    def gradeChangeToDouble(self, grade):
+        if grade == "AA":
+            return 4
+        elif grade == "BA":
+            return 3.5
+        elif grade == "BB":
+            return 3
+        elif grade == "CB":
+            return 2.5
+        elif grade == "CC":
+            return 2
+        elif grade == "DC":
+            return 1.5
+        elif grade == "DD":
+            return 1
+        elif grade == "FD":
+            return 0.5
+        elif grade == "FF":
+            return 0
