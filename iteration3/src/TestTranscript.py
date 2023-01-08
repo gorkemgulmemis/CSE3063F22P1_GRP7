218685a0f3a35799d5dcbc9461877b98a1388ad5
Muhammet Görkem Gülmemiş <118111528+gorkemgulmemis@users.noreply.github.com>
2023-01-08 23:30:29 +0300

    Update TestTranscript.py

--- iteration3/src/TestTranscript.py
+++ iteration3/src/TestTranscript.py
@@ -1,1 +1,47 @@
+import unittest
 
+class TestTranscript (unittest.TestCase):
+
+#This method tests the method in which the student's overall academic average is 
+#calculated by converting the letter grade received by the student to the double number value."""
+
+
+    def test_grade_change_to_double(self):
+    # create a test object
+     test_grade = TestTranscript()
+
+    # test the AA case
+     result = test_grade.grade_change_to_double("AA")
+     self.assertEqual(result, 4)
+
+    # test the BA case
+     result = test_grade.grade_change_to_double("BA")
+     self.assertEqual(result, 3.5)
+
+    # test the BB case
+     result = test_grade.grade_change_to_double("BB")
+     self.assertEqual(result, 3)
+
+    # test the CB case
+     result = test_grade.grade_change_to_double("CB")
+     self.assertEqual(result, 2.5)
+
+    # test the CC case
+     result = test_grade.grade_change_to_double("CC")
+     self.assertEqual(result, 2)
+
+    # test the DC case
+     result = test_grade.grade_change_to_double("DC")
+     self.assertEqual(result, 1.5)
+
+    # test the DD case
+     result = test_grade.grade_change_to_double("DD")
+     self.assertEqual(result, 1)
+
+    # test the FD case
+     result = test_grade.grade_change_to_double("FD")
+     self.assertEqual(result, 0.5)
+
+    # test the FF case
+     result = test_grade.grade_change_to_double("FF")
+     self.assertEqual(result, 0)
