fcf73ab98d1c0928ee1daa93db8d79275485e9c3
mervanabursu <93670475+mervanabursu@users.noreply.github.com>
2023-01-08 23:36:07 +0300

    Update Transcript.py

--- iteration3/src/Transcript.py
+++ iteration3/src/Transcript.py
@@ -18,3 +18,21 @@
             return 0.5
         elif grade == "FF":
             return 0
+    
+    def printStudentInfoOnConsole(self, student = None, advisor= None, course= None, gano= None, yano = None):
+        print("----- Students are created! -----")
+        print("For Example: ")
+        print()
+        print("Student Name: " + student.getName())
+        print("Student Number: " + str(student.getStudentNumber()))
+        print("Student Year: " + str(student.getYear()))
+        print("Student Semester: " + str(student.getSemester()))
+        print("Student Department: " + student.getDepartment())
+        print("Student Title: " + student.getTitle())
+        print("Student Yano: " + str(yano))
+        print("Student Gano: " + str(gano))
+        print("Student Advisor Name and Title: " + advisor.getTitle() + " " + advisor.getName())
+        print()
+        print("Last Semester Course Names: ")
+        for i, c in enumerate(course.getLastSemesterCourseList()):
+            print(i + 1, c.course_name)
