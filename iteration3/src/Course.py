352c3b218a9208b7d96ec87592a5f64e25f4a022
Tarık Işın <118109974+TarikIsin@users.noreply.github.com>
2023-01-08 23:20:57 +0300

    Update Course.py

--- iteration3/src/Course.py
+++ iteration3/src/Course.py
@@ -1,1 +1,13 @@
 
+    def takeElectiveCoursesFromInputFile(self):
+        with open("C:\\Users\\90554\\PycharmProjects\\OOPProject\\input.json") as file:
+            data = json.load(file)
+            for key in ["NTE", "ENG-UE", "TE", "ENG-FTE"]:
+                array = data[key]
+                for j in range(len(array)):
+                    object = array[j]
+                    courseName = object["courseName"]
+                    courseCredit = object["courseCredit"]
+                    self.setCourseName(courseName)
+                    self.setCourseCredit(courseCredit)
+                    self.__addElectiveCourseList(self)
